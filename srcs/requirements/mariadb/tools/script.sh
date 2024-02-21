bff26d9e1657cecc2cc566ef376565ac9c358c71
Pomprab BUREERAK <pbureera@paul-f3Br3s5.clusters.42paris.fr>
2024-02-21 14:11:55 +0100

    OK

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 service mysql start;
 
 sleep 2
---
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sleep   10
+
+wp  config create	--allow-root \
+					--dbname=$MYSQL_DATABASE \
+					--dbuser=$MYSQL_USER \
+					--dbpass=$MYSQL_PASSWORD \
+					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+wp core install --allow-root --path=/var/www/html/wordpress \
+                --url=https://localhost/ \
+	            --title=Inception \
+	            --admin_user=$WP_ADMIN_NAME \
+	            --admin_password=$WP_ADMIN_PASSWORD \
+	            --admin_email=$WP_ADMIN_EMAIL \
+	            --skip-email
+
+wp user create --allow-root --path=/var/www/html/wordpress $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
+
+mkdir   -p /run/php
+exec    /usr/sbin/php-fpm7.3 -F
